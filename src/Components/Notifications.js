c7e982323af50af0ad4dd0f9246180124bf60e8c
Olmo Ziarko <olmo@ziarko.be>
2018-05-10 23:31:14 +0200

    moving css style

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -3,17 +3,30 @@ import { connect } from 'react-redux'
 import { Transition } from 'react-transition-group'
 import { markNotificationAsRead } from 'store/notifications'
 
+const duration = 300;
+
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+}
+
+const transitionStyles = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
+};
+
 const Notifications = ({ notifications, clickHandler }) => (
     <div id="notifications">
         {notifications.map(notification =>
             <Transition
                 key={notification.id}
                 in={!notification.read}
-                timeout={150}
+                timeout={duration}
                 unmountOnExit>
                 {state =>
-                    <div className={`notification ${state}`}
-                        onClick={clickHandler(notification.id)}>
+                    <div
+                        onClick={clickHandler(notification.id)}
+                        style={{ ...defaultStyle, ...transitionStyles[state] }}>
                         {notification.content}
                     </div>}
             </Transition>
