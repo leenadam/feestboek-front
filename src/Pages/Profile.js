759aa153fc6b8f1849b47935e558ca34f370c5ad
Olmo Ziarko <olmo@ziarko.be>
2018-05-11 05:27:43 +0200

    mock text

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -3,12 +3,37 @@ import { connect } from 'react-redux'
 
 import DeregisterForm from 'Components/DeregisterForm'
 
-const Profile = ({ dispatch }) => (
+const Profile = ({ username }) => (
     <main>
-        <h1>Profile</h1>
+        <h1>Welcome, {username}!</h1>
+        <p>What would you like to do?</p>
+        <ul>
+            <li>Mike posted 3 pictures in <b>HORI2ON DU T3K AFTERPARTY!</b></li>
+            <li>Mike posted 54 pictures in <b>HORI2ON DU T3K!</b></li>
+            <li>Mike commented on <b>HORI2ON DU T3K!</b></li>
+            <li>Party ended with 163 attendees: <b>HORI2ON DU T3K!</b></li>
+            <li>Party started: <b>HORI2ON DU T3K! AFTERPARTY</b></li>
+            <li>Mike shared <b>HORI2ON DU T3K! AFTERPARTY</b> to <b>HORI2ON DU T3K!</b></li>
+            <li>Mike created a new party: <b>HORI2ON DU T3K! AFTERPARTY</b></li>
+            <li>Mike commented on <b>HORI2ON DU T3K!</b></li>
+            <li>Mike commented on <b>HORI2ON DU T3K!</b></li>
+            <li>Alex update the location of <b>HORI2ON DU T3K!</b></li>
+            <li>Party started: <b>HORI2ON DU T3K!</b></li>
+            <li>Mike invited you to <b>HORI2ON DU T3K!</b></li>
+            <li>Mike will attend <b>HORI2ON DU T3K!</b></li>
+            <li>Mike commented on <b>HORI2ON DU T3K!</b></li>
+            <li>Alex created a new party: <b>HORI2ON DU T3K!</b></li>
+        </ul>
+
         <h1>Delete account</h1>
+        <p>Warning, this will permanently delete your account. Your invitation code remains invalidated.</p>
         <DeregisterForm />
     </main>
 )
 
-export default connect()(Profile)
+const mapStateToProps = state => ({
+    username: state.user.profile.username,
+})
+
+
+export default connect(mapStateToProps)(Profile)
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -7,6 +7,14 @@ import RegisterForm from 'Components/RegisterForm'
 const Register = ({ dispatch }) => (
     <main>
         <h1>Register</h1>
+        <p>Welcome to the Feestboek!</p>
+        <ul>
+            <li>All data on the Feestboek is encrypted using <code>tweetnacl-js</code>.</li>
+            <li>Your private key is encrypted with a password before it is stored on the server.</li>
+            <li>Choose a LONG password (a full sentence works).</li>
+            <li>We can not restore your access if you forget your password.</li>
+        </ul>
+        <p>Feestboek is currently is closed beta, you need an invitation code to join.</p>
         <RegisterForm onRegister={() => dispatch(push("/profile"))} />
     </main>
 )
